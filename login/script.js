3e782342e4ca8a7d77acf547094ff6020fa06f04
rrbrink <ryanbrinkerhoff@live.com>
2018-01-13 10:12:47 -0700

    login work

---
+++ login/script.js
@@ -0,0 +1,12 @@
+
+var forgotPassword = $('passRecover').val().trim()
+var attempts = 4
+
+function login(){
+$('#submitBtn').on("clink", function(){
+    var userName = $('#userName').val().trim();
+    var password = $('#password').val().trim();
+    export{userName, password}
+})
+
+}
