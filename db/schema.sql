972b5b8ae249ffd986f0bcec9e9f1f1e895538e6
zz0115 <{zz0115}@users.noreply.github.com>
2018-01-13 10:28:54 -0700

    empty folders

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+### Schema
+
+CREATE DATABASE restaurants_db;
+USE restaurants_db;
+
+CREATE TABLE restaurants
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	address varchar(255) NOT NULL,
+	typeOfFood varchar(255) NOT NULL,
+	rate FLOAT(1),
+	price varchar(255) NOT NULL, 
+	-- picture
+	PRIMARY KEY (id)
+);
