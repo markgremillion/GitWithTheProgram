987bca3259e3fb7ecd5f368242b6e7aafa2e7ac7
zz0115 <zhangziwei0115@gmail.com>
2018-01-18 20:47:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -38,8 +38,6 @@ app.use(express.static("public"));
 
 require("./routes/restaurant-api-routes.js")(app);
 
-require("./routes/api-routes.js")(app);
-
 require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
