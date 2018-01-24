887c324c833ca858e03761ca50f6a3629a836184
markgremillion <markgremillion@gmail.com>
2018-01-23 19:11:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ require("./routes/html-routes.js")(app);
 
 //force true: wipe out database every time when restart
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
